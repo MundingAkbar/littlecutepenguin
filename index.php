1d885661674f24e40a915c2399a44948032ea05d
MundingAkbar <sl201501058@wmsu.edu.ph>
2021-06-12 17:28:52 +0800

    .

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Little Cute Penguin</title>
+
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js" integrity="sha256-XL2inqUJaslATFnHdJOi9GfQ60on8Wx1C2H8DYiN1xY=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Coming soon...</h1>
+        <h3 id="maintenance">Website Under Development</h3>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
+    <script>
+             new TypeIt("#maintenance", {
+                speed: 80,
+                loop: true,
+                waitUntilVisible: true
+            }).go();
+    </script>
+</body>
+</html>
