cd88877c6ef8241f8acc447f38768d2271914324
MundingAkbar <sl201501058@wmsu.edu.ph>
2021-06-12 20:29:28 +0800

    .

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Little Cute Penguin</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.2">
     <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js" integrity="sha256-XL2inqUJaslATFnHdJOi9GfQ60on8Wx1C2H8DYiN1xY=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
         <h1>Coming soon...</h1>
         <h3 id="maintenance"></h3>
+        <img id="logo" src="pengiun.png" alt="penguin">
     </div>
     <script src="https://cdn.jsdelivr.net/npm/typeit@7.0.4/dist/typeit.min.js"></script>
     <script>
